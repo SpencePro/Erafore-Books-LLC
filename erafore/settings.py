269b091aeaadd65db4b484abf07d444776592e8c
SpencePro <spencer.a.rodgers@gmail.com>
2021-09-22 16:33:21 -0500

    update database

--- erafore/settings.py
+++ erafore/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True #os.getenv('DEBUG_VALUE', '') != 'False'
+DEBUG = False #os.getenv('DEBUG_VALUE', '') != 'False'
 
 ALLOWED_HOSTS = ['eraforebooksllc.herokuapp.com']
 
