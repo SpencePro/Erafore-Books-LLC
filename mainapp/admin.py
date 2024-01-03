b051a5eb48101fa506366ab76db899c79f901321
Spencer Rodgers <62363257+SpencePro@users.noreply.github.com>
2024-01-02 18:05:30 -0600

    Update admin.py
    add export books action

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
-from mainapp.models import Book, Series, LoreObject, World
+from mainapp.models import Book, Series, LoreObject
+import csv
+from django.http import HttpResponse
+
+def export_selected_books(modeladmin, request, queryset):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="books_export.csv"'
+
+    writer = csv.writer(response)
+    writer.writerow(['id', 'title', 'cover_artist', 'image', 'series', 'world', 'amazon_link', 'date_released', 'synopsis', 'on_sale', 'audio_book'])
+
+    for book in queryset:
+        writer.writerow([book.id, book.title, book.cover_artist, book.image, book.series, book.world, book.amazon_link, book.date_released, book.synopsis, book.on_sale, book.audio_book])
+
+    return response
+
+class BookAdmin(admin.ModelAdmin):
+    actions = [export_selected_books]
+
 # Register your models here.
-admin.site.register(Book)
+admin.site.register(Book, BookAdmin)
 admin.site.register(Series)
 admin.site.register(LoreObject)
-admin.site.register(World)
-
